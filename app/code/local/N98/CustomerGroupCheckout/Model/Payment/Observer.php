0d39c3d79764e2899c42e79901ecbda207e6f248
Christian Münch <cmuench@inmon.de>
2011-06-17 12:03:57 +0200

    Updated license docblock

--- app/code/local/N98/CustomerGroupCheckout/Model/Payment/Observer.php
+++ app/code/local/N98/CustomerGroupCheckout/Model/Payment/Observer.php
@@ -1,34 +1,32 @@
 <?php
-/***
- * Copyright (c) 2011 netz98 new media GmbH
- * All rights reserved.
+/**
+ * Copyright © 2011 
+ * netz98 new media GmbH. All rights reserved. 
  * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * The use and redistribution of this software, either compiled or uncompiled, 
+ * with or without modifications are permitted provided that the 
+ * following conditions are met:
  * 
- * Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- * 
- * Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
- * 
- * Neither the name of the project's author nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
- * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 1. Redistributions of compiled or uncompiled source must contain the above 
+ * copyright notice, this list of the conditions and the following disclaimer: 
+ *
+ * 2. All advertising materials mentioning features or use of this software must 
+ * display the following acknowledgement: 
+ * "This product includes software developed by the netz98 new media GmbH, Mainz."
+ *
+ * 3. The name of the netz98 new media GmbH may not be used to endorse or promote 
+ * products derived from this software without specific prior written permission.
+ *
+ * 4. License holders of the netz98 new media GmbH are only permitted to 
+ * redistribute altered software, if this is licensed under conditions that contain 
+ * a copyleft-clause. 
+ *
+ * This software is provided by the netz98 new media GmbH without any express or 
+ * implied warranties. netz98 is under no condition liable for the functional 
+ * capability of this software for a certain purpose or the general usability. 
+ * netz98 is under no condition liable for any direct or indirect damages resulting 
+ * from the use of the software. 
+ * Liability and Claims for damages of any kind are excluded. 
  */
 
 /**
