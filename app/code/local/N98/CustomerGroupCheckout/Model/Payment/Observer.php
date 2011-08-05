2a8593896a21b6fd15b13ee8f0b90be76cb052d6
Christian Münch <cmuench@inmon.de>
2011-08-05 14:32:37 +0200

    Renamed module names from Netz98_ to N98

--- app/code/local/N98/CustomerGroupCheckout/Model/Payment/Observer.php
+++ app/code/local/N98/CustomerGroupCheckout/Model/Payment/Observer.php
@@ -1,32 +1,32 @@
 <?php
 /**
- * Copyright © 2011 
- * netz98 new media GmbH. All rights reserved. 
- * 
- * The use and redistribution of this software, either compiled or uncompiled, 
- * with or without modifications are permitted provided that the 
- * following conditions are met:
- * 
- * 1. Redistributions of compiled or uncompiled source must contain the above 
- * copyright notice, this list of the conditions and the following disclaimer: 
+ * Copyright © 2011
+ * netz98 new media GmbH. All rights reserved.
  *
- * 2. All advertising materials mentioning features or use of this software must 
- * display the following acknowledgement: 
+ * The use and redistribution of this software, either compiled or uncompiled,
+ * with or without modifications are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of compiled or uncompiled source must contain the above
+ * copyright notice, this list of the conditions and the following disclaimer:
+ *
+ * 2. All advertising materials mentioning features or use of this software must
+ * display the following acknowledgement:
  * "This product includes software developed by the netz98 new media GmbH, Mainz."
  *
- * 3. The name of the netz98 new media GmbH may not be used to endorse or promote 
+ * 3. The name of the netz98 new media GmbH may not be used to endorse or promote
  * products derived from this software without specific prior written permission.
  *
- * 4. License holders of the netz98 new media GmbH are only permitted to 
- * redistribute altered software, if this is licensed under conditions that contain 
- * a copyleft-clause. 
+ * 4. License holders of the netz98 new media GmbH are only permitted to
+ * redistribute altered software, if this is licensed under conditions that contain
+ * a copyleft-clause.
  *
- * This software is provided by the netz98 new media GmbH without any express or 
- * implied warranties. netz98 is under no condition liable for the functional 
- * capability of this software for a certain purpose or the general usability. 
- * netz98 is under no condition liable for any direct or indirect damages resulting 
- * from the use of the software. 
- * Liability and Claims for damages of any kind are excluded. 
+ * This software is provided by the netz98 new media GmbH without any express or
+ * implied warranties. netz98 is under no condition liable for the functional
+ * capability of this software for a certain purpose or the general usability.
+ * netz98 is under no condition liable for any direct or indirect damages resulting
+ * from the use of the software.
+ * Liability and Claims for damages of any kind are excluded.
  */
 
 /**
@@ -50,7 +50,6 @@ class N98_CustomerGroupCheckout_Model_Payment_Observer
      */
     public function methodIsAvailable(Varien_Event_Observer $observer)
     {
-        $quote = $observer->getQuote();
         $paymentMethodInstance = $observer->getMethodInstance();
         /* @var $paymentMethodInstance Mage_Payment_Model_Method_Abstract */
         $result = $observer->getResult();
